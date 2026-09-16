37845f92ed43a851f0d9b8905c0bbe28d850b395
KURO <167153557+KURO-2000@users.noreply.github.com>
2026-09-16 16:45:04 +0900

    Update hotel guide from admin

--- data.js
+++ data.js
@@ -433,12 +433,6 @@ const SITE_DATA = {
           "mapQuery": "お好み焼き 弐八, Osaka",
           "mapUrl": "https://maps.app.goo.gl/d7gvQnuSbzCsBi4b6",
           "image": "assets/restaurants/okonomiyaki-4.jpg"
-        },
-        {
-          "name": "test",
-          "mapQuery": "test",
-          "mapUrl": "test",
-          "image": "assets/uploads/1789544598879-breakfast-1.jpg"
         }
       ],
       "sushi": [
@@ -707,12 +701,6 @@ const SITE_DATA = {
           "mapQuery": "お好み焼き 弐八, Osaka",
           "mapUrl": "https://maps.app.goo.gl/d7gvQnuSbzCsBi4b6",
           "image": "assets/restaurants/okonomiyaki-4.jpg"
-        },
-        {
-          "name": "test",
-          "mapQuery": "test",
-          "mapUrl": "test",
-          "image": "assets/uploads/1789544598879-breakfast-1.jpg"
         }
       ],
       "sushi": [
